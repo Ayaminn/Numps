ba2dac33a9b35b8bb204837eee6a880f913726ce
Ayami_Imai <ayami0727tu@yahoo.co.jp>
2015-03-28 18:50:37 +0900

    初期投稿

---
+++ Assets/Medieval_House/Scripts/OpenDoor.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var doorObject:GameObject;
+var openedDoor:boolean;
+function Start () {
+	openedDoor=true;
+}
+
+function Update () {
+	
+}
+
+/*function OnCollisionEnter(other:collider){
+	if (openedDoor){
+		doorObject.animation.Play();
+		openedDoor=false;
+		Debug.Log("open");
+	}*/
+//}
